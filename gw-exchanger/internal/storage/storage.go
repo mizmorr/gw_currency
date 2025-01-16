042b56f1f51c5c44d4c6d6ce1030b55c55283fab
mizmorr <mizmorrr@gmail.com>
2025-01-16 22:59:59 +0300

    upd: add storage interface

---
+++ gw-exchanger/internal/storage/storage.go
@@ -0,0 +1,15 @@
+package storage
+
+import (
+	"context"
+
+	"github.com/mizmorr/gw_currency/gw-exchanger/internal/storage/model"
+)
+
+type Repository interface {
+	GetAllRates(ctx context.Context) ([]*model.Rate, error)
+	GetRate(ctx context.Context, code string) (*model.Rate, error)
+
+	Start(ctx context.Context) error
+	Stop(ctx context.Context)
+}
