476313649f7adebdeda5be746eb52d21be412a63
mizmorr <mizmorrr@gmail.com>
2025-01-19 21:01:17 +0300

    upd: new methods for postgres repo

--- gw-currency-wallet/internal/store/postgres/methods.go
+++ gw-currency-wallet/internal/store/postgres/methods.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"time"
 
 	"github.com/mizmorr/gw_currency/gw-currency-wallet/internal/store"
 	"github.com/mizmorr/gw_currency/gw-currency-wallet/pkg/hasher"
@@ -44,4 +45,61 @@ func (repo *PostgresRepo) CreateUser(ctx context.Context, user *store.User) erro
 	}
 
 	return nil
+}
+
+func (repo *PostgresRepo) CheckPassword(ctx context.Context, user *store.User) error {
+	var (
+		sql      = "SELECT password from USERS WHERE username = $1"
+		password string
+	)
+
+	err := repo.db.QueryRow(ctx, sql, user.Username).Scan(&password)
+	if err != nil {
+		return errors.Wrap(err, "invalid credentials")
+	}
+
+	if !hasher.CheckPassword(user.Password, password) {
+		return errors.New("password is not correct")
+	}
+
+	return nil
+}
+
+func (repo *PostgresRepo) AuthenticateUser(ctx context.Context, refresh *store.RefreshToken) error {
+	err := repo.createRefreshToken(ctx, refresh.ExpiresAt, refresh.RefreshHash, refresh.UserID)
+	if err != nil {
+		return errors.Wrap(err, "invalid refresh token")
+	}
+
+	return nil
+}
+
+func (repo *PostgresRepo) createRefreshToken(ctx context.Context, expTime time.Time, refreshToken string, userid int64) error {
+	sql := `INSERT into refresh_tokens(user_id,refresh_hash,expires_at, revoked) VALUES
+	($1,$2,$3,false)`
+	_, err := repo.db.Exec(ctx, sql, userid, refreshToken, expTime)
+	return err
+}
+
+func (repo *PostgresRepo) GetBalance(ctx context.Context, userid int64) ([]*store.WalletBalance, error) {
+	var (
+		sql     = `select wallet_balances.id,wallet_balances.currency,wallet_balances.balance from wallet_balances join wallets on wallet_balances.wallet_id=wallets.id where wallets.id=$1`
+		wallets []*store.WalletBalance
+		wallet  store.WalletBalance
+	)
+	rows, err := repo.db.Query(ctx, sql, userid)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to query wallet balances")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&wallet.ID, &wallet.Currency, &wallet.Balance)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to scan row")
+		}
+		wallets = append(wallets, &wallet)
+	}
+
+	return wallets, nil
 }
