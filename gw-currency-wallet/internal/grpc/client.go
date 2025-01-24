1bffc69c6232f3ee251570e45587c141452864a8
mizmorr <mizmorrr@gmail.com>
2025-01-24 17:38:52 +0300

    upd: typo fix

--- gw-currency-wallet/internal/grpc/client.go
+++ gw-currency-wallet/internal/grpc/client.go
@@ -5,8 +5,6 @@ import (
 	"time"
 
 	pb "github.com/mizmorr/grpc_exchange/exchange"
-	"github.com/mizmorr/gw_currency/gw-currency-wallet/internal/domain"
-	"github.com/mizmorr/gw_currency/gw-currency-wallet/internal/mappers"
 	"google.golang.org/grpc"
 )
 
@@ -20,19 +18,19 @@ func NewExchangerClient(conn *grpc.ClientConn) *ExchangerClient {
 	}
 }
 
-func (c *ExchangerClient) GetAllRates() ([]*domain.RateResponse, error) {
+func (c *ExchangerClient) GetAllRates() (*pb.ExchangeRatesResponse, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	rate, err := c.client.GetAllRates(ctx, &pb.EmptyRequest{})
+	rates, err := c.client.GetAllRates(ctx, &pb.EmptyRequest{})
 	if err != nil {
 		return nil, err
 	}
 
-	return mappers.ToDomainRatesResponse(rate), nil
+	return rates, nil
 }
 
-func (c *ExchangerClient) GetSpecificRate(code string) (*domain.RateResponse, error) {
+func (c *ExchangerClient) GetSpecificRate(code string) (*pb.ExchangeRateResponse, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
@@ -41,5 +39,5 @@ func (c *ExchangerClient) GetSpecificRate(code string) (*domain.RateResponse, er
 		return nil, err
 	}
 
-	return mappers.ToDomainRateResponse(rate), nil
+	return rate, nil
 }
