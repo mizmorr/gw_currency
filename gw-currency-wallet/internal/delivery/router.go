4095e98d204dd274788c71489e60115edd7cd9a5
mizmorr <mizmorrr@gmail.com>
2025-01-27 11:45:46 +0300

    upd: make router

---
+++ gw-currency-wallet/internal/delivery/router.go
@@ -0,0 +1,42 @@
+package delivery
+
+import (
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+type Controller interface {
+	Register(c *gin.Context)
+	Login(c *gin.Context)
+	GetBalance(c *gin.Context)
+	Deposit(c *gin.Context)
+	Withdraw(c *gin.Context)
+	Refresh(c *gin.Context)
+	ExchangeRatesHandler(c *gin.Context)
+	ExchangeHandler(c *gin.Context)
+}
+
+func NewRouter(router *gin.Engine, authMiddleware gin.HandlerFunc, c Controller) {
+	router.Use(gin.Recovery())
+	router.Use(gin.Logger())
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	publicRoutes := router.Group("/api/v1")
+	{
+		publicRoutes.POST("/register", c.Register)
+		publicRoutes.POST("/login", c.Login)
+		publicRoutes.POST("/refresh", c.Refresh)
+	}
+
+	protectedRoutes := router.Group("/api/v1/wallet")
+	protectedRoutes.Use(authMiddleware)
+	{
+		protectedRoutes.GET("/balance", c.GetBalance)
+		protectedRoutes.POST("/deposit", c.Deposit)
+		protectedRoutes.POST("/withdraw", c.Withdraw)
+
+		protectedRoutes.GET("/exchange-rates", c.ExchangeRatesHandler)
+		protectedRoutes.POST("/exchange", c.ExchangeHandler)
+	}
+}
